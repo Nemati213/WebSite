dc13f6828f7e4322e7e95501ba6338f6804be530
Nemati213 <147091630+Nemati213@users.noreply.github.com>
2023-12-15 18:51:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-import io
 import streamlit as st
 from PIL import Image
-
 import keras
 import numpy as np
-import cv2
-from keras.applications import  VGG19
-
-import pathlib
+from keras.applications import VGG19
+import requests
 import os
-from tempfile import NamedTemporaryFile
-
 
 def PreprocessAndPredict(image):
-    # Define list of class names
+    # Определение списка названий классов
     class_names = ["Acne", "Eczema", "Atopic", "Psoriasis", "Tinea", "Vitiligo"]
 
-    # Load saved model
-    model = keras.models.load_model("C:\\Users\\User\\Downloads\\6claass.h5")
-    vgg_model = VGG19(weights='imagenet', include_top=False, input_shape=(180, 180, 3))
+    # Получение модели из репозитория GitHub
+    model_url = 'https://github.com/Nemati213/Test/raw/main/6claass.h5'
+    response = requests.get(model_url)
 
-    # Load and preprocess image
-    img = cv2.imread(image)
-    img = cv2.resize(img, (180, 180))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-    img = vgg_model.predict(img)
-    img = img.reshape(1, -1)
+    # Проверка статуса запроса
+    if response.status_code == 200:
+        # Сохранение модели во временный файл
+        with open("model.h5", "wb") as file:
+            file.write(response.content)
 
-    # Make prediction on preprocessed image
-    pred = model.predict(img)[0]
-    predicted_class_index = np.argmax(pred)
-    predicted_class_name = class_names[predicted_class_index]
+        # Загрузка сохраненной модели
+        model = keras.models.load_model("model.h5")
+        vgg_model = VGG19(weights='imagenet', include_top=False, input_shape=(180, 180, 3))
 
-    return predicted_class_name
+        # Загрузка и предобработка изображения
+        img = Image.open(image)
+        img = img.resize((180, 180))
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+        img = vgg_model.predict(img)
+        img = img.reshape(1, -1)
+
+        # Предсказание на предобработанном изображении
+        pred = model.predict(img)[0]
+        predicted_class_index = np.argmax(pred)
+        predicted_class_name = class_names[predicted_class_index]
+
+        # Удаление временного файла модели
+        os.remove("model.h5")
+
+        return predicted_class_name
+    else:
+        st.write("Не удалось загрузить модель")
+        return None
 
 def load_image():
     """Создание формы для загрузки изображения"""
@@ -42,31 +52,25 @@ def load_image():
         label='Выберите изображение для распознавания')
     if uploaded_file is not None:
         # Получение загруженного изображения
-        image_data = uploaded_file.getvalue()
-        # Показ загруженного изображения на Web-странице
-        st.image(image_data)
         image = Image.open(uploaded_file)
-        image = image.save("img.jpg")
+        # Показ загруженного изображения на Web-странице
+        st.image(image)
+        image.save("img.jpg")
 
-        # Возврат изображения в формате PIL
-        return image
+        # Возврат пути к изображению
+        return "img.jpg"
     else:
         return None
 
-
-# Выводим заголовок страницы
+# Вывод заголовка страницы
 st.title('Классификатор кожных заболеваний')
-# Вызываем функцию создания формы загрузки изображения
-img = load_image()
-st.write(img)
+# Вызов функции для загрузки изображения
+img_path = load_image()
 
+# Добавление кнопки-команды
+if img_path:
+    result = st.button("Распознать изображение")
 
-
-img2 = (os.path.abspath(path=str(img)))
-st.write(img2)
-# Добавим кнопку-команду
-result = st.button("Распознать изображение")
-
-if result:
-    st.write('**Результат распознования:**')
-    st.write(PreprocessAndPredict("img.jpg"))
+    if result:
+        st.write('**Результат распознования:**')
+        st.write(PreprocessAndPredict(img_path))
